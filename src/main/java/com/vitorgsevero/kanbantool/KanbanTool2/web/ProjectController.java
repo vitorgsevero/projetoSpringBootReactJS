b9f1af7c786bc7c287c290377651da733f741c9c
vitorgsevero <vitorgsevero@gmail.com>
2019-06-20 20:34:01 -0300

    Project Object Validation: PART 2

--- src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/web/ProjectController.java
+++ src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/web/ProjectController.java
@@ -1,11 +1,16 @@
 package com.vitorgsevero.kanbantool.KanbanTool2.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +30,16 @@ public class ProjectController {
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
 		if(result.hasErrors()) {
-			return new ResponseEntity<String>("Invalid new project object!", HttpStatus.BAD_REQUEST);
+			
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for(FieldError error:  result.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage()); // I just need to show field e message
+			}	
+			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
 		}
+		
+		
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
