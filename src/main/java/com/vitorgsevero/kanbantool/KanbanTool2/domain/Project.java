b3f4b67403c9b7dffc6f68ad14a6c5b97b9a7c23
vitorgsevero <vitorgsevero@gmail.com>
2019-06-20 17:56:55 -0300

    Project Object Validation: Set up

--- src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/Project.java
+++ src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/Project.java
@@ -2,12 +2,17 @@ package com.vitorgsevero.kanbantool.KanbanTool2.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -15,14 +20,24 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	@NotBlank(message = "Project name is required")
 	private String projectName;
+	
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min=4, max=5, message = "Please use 4 or 5 characteres")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "Project description is required")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
 	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	public Project() {
