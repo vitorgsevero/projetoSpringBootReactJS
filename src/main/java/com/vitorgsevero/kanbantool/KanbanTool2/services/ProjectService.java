75bf6006514f13af976eb50b94560d6ecfd053bd
vitorgsevero <vitorgsevero@gmail.com>
2019-06-21 21:05:01 -0300

    Custom Exceptions for Unique Project Identifiers

--- src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/services/ProjectService.java
+++ src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitorgsevero.kanbantool.KanbanTool2.domain.Project;
+import com.vitorgsevero.kanbantool.KanbanTool2.exceptions.ProjectIdException;
 import com.vitorgsevero.kanbantool.KanbanTool2.repositories.ProjectRepository;
 
 @Service
@@ -13,8 +14,14 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project saveOrUpdateProject(Project project) {
-		//insert some logic here
-		return projectRepository.save(project);
+		
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch(Exception e) {
+			throw new ProjectIdException("Project ID '"+ project.getProjectIdentifier().toUpperCase()+"' already exists.");
+		}
+	
 	}
 
 }
