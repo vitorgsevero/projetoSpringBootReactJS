d3a4099967d91b41c4f6c769925527c903f6ffc2
vitorgsevero <vitorgsevero@gmail.com>
2019-06-22 21:07:40 -0300

    Style our Dashboard, Navbar and ProjectItem

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,13 +5,25 @@ class Dashboard extends Component {
     render() {
         return (
 
-            <div>
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                                Create a Project
+                        </a>
+                            <br />
+                            <hr />
 
-                <h1 className="alert alert-warning"> Welcome to the Dashboard </h1>
+                            <ProjectItem />
 
-                <ProjectItem />
-
+                        </div>
+                    </div>
+                </div>
             </div>
+
 
 
 
