7ce64966c8750ed7996da40c3de75862dc0a1156
vitorgsevero <vitorgsevero@gmail.com>
2019-06-29 20:55:18 -0300

    MySQL Set Up instead H2

--- KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/KanbanTool2Application.java
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/KanbanTool2Application.java
@@ -1,5 +1,9 @@
 package com.vitorgsevero.kanbantool.KanbanTool2;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KanbanTool2Application {
 	
+    @PostConstruct
+    void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanTool2Application.class, args);
 
