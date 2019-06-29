f24ba9028768c7e139c19a5afbe0f1075fd6c836
vitorgsevero <vitorgsevero@gmail.com>
2019-06-29 14:02:03 -0300

    Entity Relationships: Backlog and Project Tasks

--- KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/Backlog.java
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/Backlog.java
@@ -1,11 +1,16 @@
 package com.vitorgsevero.kanbantool.KanbanTool2.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -29,51 +34,52 @@ public class Backlog {
 	
 	
 	//OneToMany tasks
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+	private List<ProjectTask> projectTasks = new ArrayList<>();
 	
 	
 	
 	public Backlog() {
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public Integer getPTSequence() {
 		return PTSequence;
 	}
-
 
 	public void setPTSequence(Integer pTSequence) {
 		PTSequence = pTSequence;
 	}
 
-
 	public String getProjectIdentifier() {
 		return projectIdentifier;
 	}
-
 
 	public void setProjectIdentifier(String projectIdentifier) {
 		this.projectIdentifier = projectIdentifier;
 	}
 
-
 	public Project getProject() {
 		return project;
 	}
 
-
 	public void setProject(Project project) {
 		this.project = project;
 	}
-		
+
+	public List<ProjectTask> getProjectTasks() {
+		return projectTasks;
+	}
+
+	public void setProjectTasks(List<ProjectTask> projectTasks) {
+		this.projectTasks = projectTasks;
+	}		
 	
 }
--- KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/ProjectTask.java
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/ProjectTask.java
@@ -2,14 +2,20 @@ package com.vitorgsevero.kanbantool.KanbanTool2.domain;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ProjectTask {
@@ -29,6 +35,10 @@ public class ProjectTask {
 	private String priority;
 	
 	//ManyToOne with Backlog
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	@JoinColumn(name="backlog_id", updatable = false, nullable = false)
+	@JsonIgnore
+	private Backlog backlog;
 	
 	@Column(updatable = false)
 	private String projectIdentifier;
@@ -118,6 +128,14 @@ public class ProjectTask {
 
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
+	}
+	
+	public Backlog getBacklog() {
+		return backlog;
+	}
+
+	public void setBacklog(Backlog backlog) {
+		this.backlog = backlog;
 	}
 
 	@PrePersist
