23c08624d0408b35d880aaf23527a97d4d30e0c8
vitorgsevero <vitorgsevero@gmail.com>
2019-06-30 18:44:36 -0300

    Types and Reducers for Project Tasks

---
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -0,0 +1,32 @@
+import { GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from "../actions/types";
+
+const initalState = {
+    project_tasks: [],
+    project_task: {}
+}
+
+export default function (state = initalState, action) {
+    switch (action.type) {
+
+        case GET_BACKLOG:
+            return {
+                ...state,
+                project_tasks: action.payload
+            }
+
+        case GET_PROJECT_TASK:
+            return {
+                ...state,
+                project_task: action.payload
+            }
+
+        case DELETE_PROJECT_TASK:
+            return {
+                ...state
+                //TO DO
+            }
+
+        default:
+            return state;
+    }
+}
