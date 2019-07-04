bfb89e1d7c8c682281790978683327d04174ad36
vitorgsevero <vitorgsevero@gmail.com>
2019-07-03 22:45:26 -0300

    Initial config to Spring Security

---
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/User.java
@@ -0,0 +1,5 @@
+package com.vitorgsevero.kanbantool.KanbanTool2.domain;
+
+public class User {
+
+}
---
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/web/UserController.java
@@ -0,0 +1,5 @@
+package com.vitorgsevero.kanbantool.KanbanTool2.web;
+
+public class UserController {
+
+}
