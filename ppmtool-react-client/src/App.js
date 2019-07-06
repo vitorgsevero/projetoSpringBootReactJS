0143d91620eb1f06dc873f9236d5ccf7a0aafe0e
vitorgsevero <vitorgsevero@gmail.com>
2019-07-06 16:45:19 -0300

    Creating Login and Register Components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -11,6 +11,9 @@ import UpdateProject from './components/Project/UpdateProject';
 import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
 import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProjectTask';
+import Landing from './components/Layout/Landing';
+import Register from './components/UserManagement/Register';
+import Login from './components/UserManagement/Login';
 
 
 function App() {
@@ -19,6 +22,19 @@ function App() {
       <Router>
         <div className="App">
           <Header />
+
+          {
+            //public routes
+          }
+
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+          {
+            //private routes
+          }
+
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
