448c076f59a658711d6d17b302413d6b3784cc36
vitorgsevero <vitorgsevero@gmail.com>
2019-07-05 21:28:21 -0300

    Custom JWT Filter to use our tokens

--- KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/security/JwtTokenProvider.java
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/security/JwtTokenProvider.java
@@ -12,8 +12,13 @@ import org.springframework.stereotype.Component;
 
 import com.vitorgsevero.kanbantool.KanbanTool2.domain.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 @Component
 public class JwtTokenProvider {
@@ -47,7 +52,30 @@ public class JwtTokenProvider {
 	
 	//validate the token
 	
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        }catch (SignatureException ex){
+            System.out.println("Invalid JWT Signature");
+        }catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        }catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT token");
+        }catch (UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT token");
+        }catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
+	
 	//get the user id from token
-	
-	
+
+    public Long getUserIdFromJWT(String token){
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String)claims.get("id");
+
+        return Long.parseLong(id);
+    }
 }
--- KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/security/SecurityConfig.java
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.vitorgsevero.kanbantool.KanbanTool2.services.CustomUserDetailsService;
 
@@ -38,6 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {return  new JwtAuthenticationFilter();}
+
+    
 	@Override
 	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(customUserDetailsService);
@@ -74,6 +79,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(SIGN_UP_URLS).permitAll()
         .antMatchers(H2_URL).permitAll()
         .anyRequest().authenticated();
+		
+		
+	      http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 }
 
 	
