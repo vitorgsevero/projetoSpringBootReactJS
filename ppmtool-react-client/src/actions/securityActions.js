a7391bca5892493e941fb8beaaf7d90c7ce837a4
vitorgsevero <vitorgsevero@gmail.com>
2019-07-06 23:01:08 -0300

    User Registration

---
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { GET_ERRORS } from "../../src/actions/types";
+
+
+export const createNewUser = (newUser, history) => async dispatch => {
+
+    try {
+        await axios.post("/api/users/register", newUser);
+        history.push("/login");
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        })
+    }
+
+
+}
