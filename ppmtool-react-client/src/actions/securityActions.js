29d38a83b1ef12127a7177ac859dead11255b862
vitorgsevero <vitorgsevero@gmail.com>
2019-07-07 14:18:52 -0300

    SecurityActions and SecurityReducer

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from "../../src/actions/types";
-
+import { GET_ERRORS, SET_CURRENT_USER } from "../../src/actions/types";
+import setJWTToken from '../securityUtils/setJWTToken';
+import jwt_decode from 'jwt-decode';
 
 export const createNewUser = (newUser, history) => async dispatch => {
 
@@ -17,6 +18,26 @@ export const createNewUser = (newUser, history) => async dispatch => {
             payload: error.response.data
         })
     }
+};
 
+export const login = LoginRequest => async dispatch => {
+    try {
 
+        const res = await axios.post("/api/users/login", LoginRequest);
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        setJWTToken(token);
+        const decoded = jwt_decode(token);
+
+        dispatch({
+            type: SET_CURRENT_USER,
+            payload: decoded
+        })
+
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        })
+    }
 }
--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import errorReducer from './errorReducer';
 import projectReducer from './projectReducer';
 import backlogReducer from './backlogReducer';
+import securityRecucer from './securityReducer';
 
 export default combineReducers({
     errors: errorReducer,
     project: projectReducer,
-    backlog: backlogReducer
+    backlog: backlogReducer,
+    security: securityRecucer
 })
