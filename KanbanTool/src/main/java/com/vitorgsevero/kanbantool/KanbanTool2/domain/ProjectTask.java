e7c73d1164991e49ab0954fdc934c06a5b44bf4b
vitorgsevero <vitorgsevero@gmail.com>
2019-07-07 23:55:51 -0300

    Bug fixes

--- KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/ProjectTask.java
+++ KanbanTool/src/main/java/com/vitorgsevero/kanbantool/KanbanTool2/domain/ProjectTask.java
@@ -2,7 +2,6 @@ package com.vitorgsevero.kanbantool.KanbanTool2.domain;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -15,6 +14,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -43,8 +43,13 @@ public class ProjectTask {
 	@Column(updatable = false)
 	private String projectIdentifier;
 	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date dueDate;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	public ProjectTask() {
